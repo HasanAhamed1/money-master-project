e937ec8ad40772c719734a643ad33def38f26976
HasanAhamed1 <hasanahmed95695@gmail.com>
2022-02-18 23:14:34 +0600

    all-completed

--- script/script.js
+++ script/script.js
@@ -4,43 +4,57 @@ document
   .addEventListener("click", function () {
     const incomeInput = document.getElementById("input-income");
     const incomeInputText = incomeInput.value;
-    const getIncomeValue = parseFloat(incomeInputText);
 
     const foodInput = document.getElementById("input-food");
     const foodInputText = foodInput.value;
-    const getValue = parseFloat(foodInputText);
 
     const rentInput = document.getElementById("input-rent");
     const rentInputText = rentInput.value;
-    const getValue2 = parseFloat(rentInputText);
 
     const clothesInput = document.getElementById("input-clothes");
     const clothesInputText = clothesInput.value;
+
+    if(!(validation.isNumber(incomeInputText)) || !(validation.isNumber(foodInputText)) || !(validation.isNumber(rentInputText)) || !(validation.isNumber(clothesInputText))){
+
+     alert("Only Numbers are ALLOWED");
+    }else{
+    
+   
+    const getIncomeValue = parseFloat(incomeInputText);
+
+    
+    const getValue = parseFloat(foodInputText);
+
+   
+    const getValue2 = parseFloat(rentInputText);
+
+   
     const getValue3 = parseFloat(clothesInputText);
 
     const inputTotal = document.getElementById("total-expenses");
     var total = getValue + getValue2 + getValue3;
-    if(total < incomeInputText){
-        inputTotal.innerText = parseFloat(total);
-        const totalExpenses = inputTotal.innerText;
-        const balanceAfterExpenses = document.getElementById("balance");
-        balance = substract(getIncomeValue , totalExpenses);
-        balanceAfterExpenses.innerText = parseFloat(balance);
+    if (total < incomeInputText) {
+      inputTotal.innerText = parseFloat(total);
+      const totalExpenses = inputTotal.innerText;
+      const balanceAfterExpenses = document.getElementById("balance");
+      balance = substract(getIncomeValue, totalExpenses);
+      balanceAfterExpenses.innerText = parseFloat(balance);
 
-        foodInput.value = "";
-        rentInput.value = "";
-        clothesInput.value = "";
-    }
-    else{
-        error_message.innerText = "Expense is much more than income !";
-        incomeInput.value = "";
-        foodInput.value = "";
-        rentInput.value = "";
-        clothesInput.value = "";
-    }
-    
 
-    
+      foodInput.value = "";
+      rentInput.value = "";
+      clothesInput.value = "";
+    }
+    else {
+      error_message.innerText = "Expense is much more than income !";
+      incomeInput.value = "";
+      foodInput.value = "";
+      rentInput.value = "";
+      clothesInput.value = "";
+    }
+
+  }
+
   });
 
 document.getElementById("save-button").addEventListener("click", function () {
@@ -54,18 +68,26 @@ document.getElementById("save-button").addEventListener("click", function () {
 
   const savingAmount = document.getElementById("saving-amount");
   var saving_amount = getIncomeValue * ((getsavingValue * 1) / 100);
-  if(saving_amount<balance){
-  savingAmount.innerText = parseFloat(saving_amount);
+  if (saving_amount < balance) {
+    savingAmount.innerText = parseFloat(saving_amount);
 
-  const remainningAmount =  document.getElementById("remaining-amount");
-  remainningAmount.innerText = parseFloat(substract(balance , saving_amount));
+    const remainningAmount = document.getElementById("remaining-amount");
+    remainningAmount.innerText = parseFloat(substract(balance, saving_amount));
   }
-  else{
+  else {
     error_message2.innerText = "Saving amount is larger than balance!";
   }
 });
-function substract(first_amount,second_amount){
-    var amount = first_amount -second_amount;
-    return amount;
+function substract(first_amount, second_amount) {
+  var amount = first_amount - second_amount;
+  return amount;
 }
+
+var validation = {
+
+  isNumber:function(str) {
+      var pattern = /^\d+$/;
+      return pattern.test(str);  // returns a boolean
+  }
+};   
 
